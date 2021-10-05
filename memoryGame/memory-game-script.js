618196f59de7a69324f37fc975b10e7b5606c04c
ashokBhandaricom <91963994+ashokBhandaricom@users.noreply.github.com>
2021-10-05 20:07:37 +0530

    time limit was too short

--- memoryGame/memory-game-script.js
+++ memoryGame/memory-game-script.js
@@ -135,7 +135,7 @@ cardsChosen.push(cardArray[cardId].name)
 cardsChosenId.push(cardId) ;
 this.setAttribute('src' , cardArray[cardId].img)
 if(cardsChosen.length== 2 ){
-  setTimeout(checkForMatch, 20 ) ;
+  setTimeout(checkForMatch, 100 ) ;
 }
 }
 
